9254caef384f7404c6e9c59ee9f88481ec840bba
Josh Edgar <josh@blackhatdigital.com.au>
2018-04-16 07:31:03 +1000

    add pdf non group

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -34,7 +34,7 @@ class OrderController < ApplicationController
     @patients = Patient.all
     @meal_options = MealOption.all
 
-    @patient_meals = PatientMeal.where(delivery_date: @filtered_date).select(:patient_id, :id).group(:patient_id, :id).all
+    @patient_meals = PatientMeal.where(delivery_date: @filtered_date).all
 
     #PDF
     respond_to do |format|
